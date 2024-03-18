701ebec4ad03e6f548cf144013c76758d9fa9dbf
malfiefauzanakbar <muhamadalfiefauzanakbar@gmail.com>
2024-03-18 20:53:37 +0700

    fix cors

--- routes/api.php
+++ routes/api.php
@@ -13,38 +13,39 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::middleware(['cors'])->group(function () {
+    Route::post('/register', 'AuthController@register');
+    Route::post('/login', 'AuthController@login');
+    Route::post('/logout', 'AuthController@logout');
+    Route::post('/update/{id?}', 'AuthController@update');
+    Route::get('/detail/{id?}', 'AuthController@show');
 
-Route::post('/register', array('middleware' => 'cors', 'uses' => 'AuthController@register'));
-Route::post('/login', array('middleware' => 'cors', 'uses' => 'AuthController@login'));
-Route::post('/logout', array('middleware' => 'cors', 'uses' => 'AuthController@logout'));
-Route::post('/update/{id?}', array('middleware' => 'cors', 'uses' => 'AuthController@update'));
-Route::get('/detail/{id?}', array('middleware' => 'cors', 'uses' => 'AuthController@show'));
+    Route::get('/categoryproduct', 'CategoryProductController@index');
+    Route::post('/categoryproduct', 'CategoryProductController@store');
+    Route::get('/categoryproduct/{id?}', 'CategoryProductController@show');
+    Route::post('/categoryproduct/update/{id?}', 'CategoryProductController@update');
+    Route::delete('/categoryproduct/{id?}', 'CategoryProductController@destroy');
 
-Route::get('/categoryproduct', array('middleware' => 'cors', 'uses' => 'CategoryProductController@index'));
-Route::post('/categoryproduct', array('middleware' => 'cors', 'uses' => 'CategoryProductController@store'));
-Route::get('/categoryproduct/{id?}', array('middleware' => 'cors', 'uses' => 'CategoryProductController@show'));
-Route::post('/categoryproduct/update/{id?}', array('middleware' => 'cors', 'uses' => 'CategoryProductController@update'));
-Route::delete('/categoryproduct/{id?}', array('middleware' => 'cors', 'uses' => 'CategoryProductController@destroy'));
+    Route::get('/product', 'ProductController@index');
+    Route::post('/product', 'ProductController@store');
+    Route::get('/product/{id?}', 'ProductController@show');
+    Route::post('/product/update/{id?}', 'ProductController@update');
+    Route::delete('/product/{id?}', 'ProductController@destroy');
 
-Route::get('/product', array('middleware' => 'cors', 'uses' => 'ProductController@index'));
-Route::post('/product', array('middleware' => 'cors', 'uses' => 'ProductController@store'));
-Route::get('/product/{id?}', array('middleware' => 'cors', 'uses' => 'ProductController@show'));
-Route::post('/product/update/{id?}', array('middleware' => 'cors', 'uses' => 'ProductController@update'));
-Route::delete('/product/{id?}', array('middleware' => 'cors', 'uses' => 'ProductController@destroy'));
+    Route::get('/tax', 'TaxController@index');
+    Route::post('/tax', 'TaxController@store');
+    Route::get('/tax/{id?}', 'TaxController@show');
+    Route::post('/tax/update/{id?}', 'TaxController@update');
+    Route::delete('/tax/{id?}', 'TaxController@destroy');
 
-Route::get('/tax', array('middleware' => 'cors', 'uses' => 'TaxController@index'));
-Route::post('/tax', array('middleware' => 'cors', 'uses' => 'TaxController@store'));
-Route::get('/tax/{id?}', array('middleware' => 'cors', 'uses' => 'TaxController@show'));
-Route::post('/tax/update/{id?}', array('middleware' => 'cors', 'uses' => 'TaxController@update'));
-Route::delete('/tax/{id?}', array('middleware' => 'cors', 'uses' => 'TaxController@destroy'));
+    Route::get('/cart/{userId?}', 'CartController@index');
+    Route::post('/cart', 'CartController@store');
+    Route::post('/cart/update/{id?}', 'CartController@update');
+    Route::delete('/cart/{id?}', 'CartController@destroy');
+    Route::get('/cart/count/{userId?}', 'CartController@countCart');
 
-Route::get('/cart/{userId?}', array('middleware' => 'cors', 'uses' => 'CartController@index'));
-Route::post('/cart', array('middleware' => 'cors', 'uses' => 'CartController@store'));
-Route::post('/cart/update/{id?}', array('middleware' => 'cors', 'uses' => 'CartController@update'));
-Route::delete('/cart/{id?}', array('middleware' => 'cors', 'uses' => 'CartController@destroy'));
-Route::get('/cart/count/{userId?}', array('middleware' => 'cors', 'uses' => 'CartController@countCart'));
-
-Route::get('/transaction/{userId?}', array('middleware' => 'cors', 'uses' => 'TransactionController@index'));
-Route::post('/transaction', array('middleware' => 'cors', 'uses' => 'TransactionController@store'));
-Route::get('/transaction/{userId?}/{id?}', array('middleware' => 'cors', 'uses' => 'TransactionController@show'));
+    Route::get('/transaction/{userId?}', 'TransactionController@index');
+    Route::post('/transaction', 'TransactionController@store');
+    Route::get('/transaction/{userId?}/{id?}', 'TransactionController@show');
+});
 
